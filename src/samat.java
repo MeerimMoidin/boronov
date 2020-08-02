0577f453b36868d903d9f9fe4b78e53ff68d5227
MeerimMoidin <ramissara18@gmail.com>
2020-08-01 20:31:39 -0500

    update samat.java

--- src/samat.java
+++ src/samat.java
@@ -1,5 +1,14 @@
 public class samat {
     public static void main(String[] args) {
         System.out.println("Sara and Ramis");
+        System.out.println("kkk");
+
+
+
+
+        ///
+
+
+        ///
     }
 }
